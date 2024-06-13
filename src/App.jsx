c25b3fad166fcfaea96af97a283a977af429ed44
gabrielbarbosapessoa <gabrielbarbosapessoa2@gmail.com>
2024-06-13 09:55:31 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react'
-import{Routes, Route} from 'react-router-dom'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+ 
 const App = () => {
-  const [color] = useState("#e4ff88")
+  const [color] = useState("#a54fcc")
   document.body.style.backgroundColor = color
-
+ 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
   )
 }
-
+ 
 export default App
